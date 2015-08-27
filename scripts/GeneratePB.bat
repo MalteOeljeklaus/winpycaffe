0ce91cc48503b386d6723c17ba1cef865d5208f7
unknown <oeljeklaus@MO.rst.e-technik.uni-dortmund.de>
2015-08-27 13:24:34 +0200

    * fixed windows compilation * updated 3rdparty packages including cudnn 3.0 rc
    Signed-off-by: unknown <oeljeklaus@MO.rst.e-technik.uni-dortmund.de>

--- scripts/GeneratePB.bat
+++ scripts/GeneratePB.bat
@@ -2,17 +2,24 @@ if exist ".\3rdparty" (
 	echo found 3rdparty dependencies, all good!
 ) else (
 	echo 3rdparty dependencies not found...
-	if exist ".\cudnn-6.5-win-v2.zip" (
-		call .\tools\wget_1.11.4_cygwin\wget.exe --no-check-cert https://www.dropbox.com/s/av89hy17afnwfel/3rdparty_caffe_17072015_win64.7z?dl=1 -O 3rdparty_tmp.7z
+	if exist ".\cudnn-7.0-win-x64-v3.0-rc.zip" (
+		call .\tools\wget_1.11.4_cygwin\wget.exe --no-check-cert https://www.dropbox.com/s/xtcodx7i5zlukza/3rdparty_caffe_27082015_win64.7z?dl=1 -O 3rdparty_tmp.7z
 		call .\tools\7z938-extra\7za.exe x 3rdparty_tmp.7z
 		del 3rdparty_tmp.7z
-		call .\tools\7z938-extra\7za.exe e cudnn-6.5-win-v2.zip -o3rdparty\lib cudnn-6.5-win-v2\*.lib
-		call .\tools\7z938-extra\7za.exe e cudnn-6.5-win-v2.zip -o3rdparty\bin cudnn-6.5-win-v2\*.dll
-		call .\tools\7z938-extra\7za.exe e cudnn-6.5-win-v2.zip -o3rdparty\include cudnn-6.5-win-v2\*.h
-		del cudnn-6.5-win-v2.zip
+		call .\tools\7z938-extra\7za.exe e cudnn-7.0-win-x64-v3.0-rc.zip -o3rdparty\lib cuda\lib\x64
+		call .\tools\7z938-extra\7za.exe e cudnn-7.0-win-x64-v3.0-rc.zip -o3rdparty\bin cuda\bin
+		call .\tools\7z938-extra\7za.exe e cudnn-7.0-win-x64-v3.0-rc.zip -o3rdparty\include cuda\include
+		del cudnn-7.0-win-x64-v3.0-rc.zip
+		copy "%CUDA_PATH_V7_0%\bin\cublas64_70.dll" 3rdparty\bin
+		copy "%CUDA_PATH_V7_0%\bin\cudart64_70.dll" 3rdparty\bin
+		copy "%CUDA_PATH_V7_0%\bin\curand64_70.dll" 3rdparty\bin
+::		call .\tools\7z938-extra\7za.exe e cudnn-6.5-win-v2.zip -o3rdparty\lib cudnn-6.5-win-v2\*.lib
+::		call .\tools\7z938-extra\7za.exe e cudnn-6.5-win-v2.zip -o3rdparty\bin cudnn-6.5-win-v2\*.dll
+::		call .\tools\7z938-extra\7za.exe e cudnn-6.5-win-v2.zip -o3rdparty\include cudnn-6.5-win-v2\*.h
+::		del cudnn-6.5-win-v2.zip
 	) else (
 		echo.
-		echo ### Please download cudnn-6.5-win-v2.zip to your caffe root folder to proceed ###
+		echo ### Please download cudnn-7.0-win-x64-v3.0-rc.zip to your caffe root folder to proceed ###
 		echo.
 		exit 2
 	)
