7dca3ee69ed8d2c362ea9294d2f575d1214426af
unknown <Oeljeklaus@MO.rst.e-technik.uni-dortmund.de>
2015-09-16 10:42:58 +0200

    * fixed googlenet prototxt definition files
    Signed-off-by: unknown <Oeljeklaus@MO.rst.e-technik.uni-dortmund.de>

--- examples/finetune_googlenet/convert_fcn.py
+++ examples/finetune_googlenet/convert_fcn.py
@@ -8,29 +8,29 @@ import numpy as np
 
 # make a bilinear interpolation kernel
 # credit @longjon
-#def upsample_filt(size):
-#    factor = (size + 1) // 2
-#    if size % 2 == 1:
-#        center = factor - 1
-#    else:
-#        center = factor - 0.5
-#    og = np.ogrid[:size, :size]
-#    return (1 - abs(og[0] - center) / factor) * \
-#           (1 - abs(og[1] - center) / factor)
+def upsample_filt(size):
+    factor = (size + 1) // 2
+    if size % 2 == 1:
+        center = factor - 1
+    else:
+        center = factor - 0.5
+    og = np.ogrid[:size, :size]
+    return (1 - abs(og[0] - center) / factor) * \
+           (1 - abs(og[1] - center) / factor)
 
 # set parameters s.t. deconvolutional layers compute bilinear interpolation
 # N.B. this is for deconvolution without groups
-#def interp_surgery(net, layers):
-#    for l in layers:
-#        m, k, h, w = net.params[l][0].data.shape
-#        if m != k:
-#            print 'input + output channels need to be the same'
-#            raise
-#        if h != w:
-#            print 'filters need to be square'
-#            raise
-#        filt = upsample_filt(h)
-#        net.params[l][0].data[range(m), range(k), :, :] = filt
+def interp_surgery(net, layers):
+    for l in layers:
+        m, k, h, w = net.params[l][0].data.shape
+        if m != k:
+            print 'input + output channels need to be the same'
+            raise
+        if h != w:
+            print 'filters need to be square'
+            raise
+        filt = upsample_filt(h)
+        net.params[l][0].data[range(m), range(k), :, :] = filt
 
 
 def convert_fcn(original_deploy, fcn_deploy, original_params, fcn_params, original_model_file, fcn_model_file_output):
@@ -49,11 +49,11 @@ def convert_fcn(original_deploy, fcn_deploy, original_params, fcn_params, origin
         conv_params[pr_conv][1][...] = fc_params[pr][1]
 
     # do net surgery to set the deconvolution weights for bilinear interpolation
-#    interp_layers = [k for k in net_full_conv.params.keys() if 'up' in k]
-#    interp_surgery(net_full_conv, interp_layers)
+    interp_layers = [k for k in net_full_conv.params.keys() if 'up' in k]
+    interp_surgery(net_full_conv, interp_layers)
     
     net_full_conv.save(fcn_model_file_output)
-    
+
 
 
 convert_fcn('../../models/bvlc_googlenet/deploy.prototxt','../../models/bvlc_googlenet/fcn_8stride/fcn-deploy_8stride_early.prototxt',
