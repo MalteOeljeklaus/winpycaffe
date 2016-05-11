ffb95e4ad28405087cc9ab0433e193bcc941f67c
unknown <oeljeklaus@MO.rst.e-technik.uni-dortmund.de>
2016-05-11 12:20:46 +0200

    restructured masking layer code and removed debug outputs
    TODO:
    * dual output/ loss function
    * further testing

--- src/caffe/layers/masking_layer.cpp
+++ src/caffe/layers/masking_layer.cpp
@@ -61,7 +61,6 @@ namespace caffe {
 	template <typename Dtype>
 	void MaskingLayer<Dtype>::Forward_cpu(
 		const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
-		LOG(INFO) << "MaskingLayer::Forward_cpu() called, this: " << std::to_string((long) this);
 		caffe_mul(top[0]->count(), bottom[0]->cpu_data(), this->blobs_[0]->cpu_data(), top[0]->mutable_cpu_data()); // multiply mask, y=a*b
 		if (bias_term_) {
 			caffe_axpy(top[0]->count(), (Dtype)0.0, this->blobs_[1]->cpu_data(), top[0]->mutable_cpu_data()); // y=a*x+y
@@ -71,49 +70,42 @@ namespace caffe {
 	template <typename Dtype>
 	void MaskingLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
 		const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
-
 		CHECK_GE(this->blobs_.size(), 1);
 
-		LOG(INFO) << "MaskingLayer::Backward_cpu() called, this=" << std::to_string((unsigned int) this) << ", blobs_[0].mutable_cpu_diff()=" << std::to_string((unsigned int) this->blobs_[0]->mutable_cpu_diff());
-		
 		// TODO: check gradient formulas (http://ufldl.stanford.edu/tutorial/supervised/MultiLayerNeuralNetworks/)
 
-		if (propagate_down[0]) {
-			if (stable_prod_grad_) {
-				LOG(ERROR) << "stable_prod_grad not yet implemented"; // TODO: see elementwise layer for inspiration
-
+		if (stable_prod_grad_) {
+			if (propagate_down[0]) {
 				// Gradient with respect to bottom data
 				caffe_mul(top[0]->count(), this->blobs_[0]->cpu_data(), top[0]->cpu_diff(), bottom[0]->mutable_cpu_diff()); // d_i = d_(i+1) .* w
-
-				// Gradient with respect to weights
-				caffe_mul(top[0]->count(), bottom[0]->cpu_data(), top[0]->cpu_diff(), this->blobs_[0]->mutable_cpu_diff()); // d_i = d_(i+1) .* in
-
-				// Gradient with respect to bias
-				if (bias_term_) {
-					LOG(ERROR) << "bias gradient not yet implemented"; // TODO: see elementwise layer for inspiration
-					//caffe_copy(top[0]->count(), top[0]->cpu_diff(), this->blobs_[1]->mutable_cpu_diff()); // = d_i+1
-				}
 			}
-			else {
-				// less stable gradient computation method inspired by elementwise layer, this is just for comparison/debugging purposes
 
+			// Gradient with respect to weights
+			caffe_mul(top[0]->count(), bottom[0]->cpu_data(), top[0]->cpu_diff(), this->blobs_[0]->mutable_cpu_diff()); // d_i = d_(i+1) .* in
+
+			// Gradient with respect to bias
+			if (bias_term_) {
+				LOG(ERROR) << "bias gradient not yet implemented"; // TODO: see elementwise layer for inspiration
+				//caffe_copy(top[0]->count(), top[0]->cpu_diff(), this->blobs_[1]->mutable_cpu_diff()); // = d_i+1
+			}
+		} else {
+			// less stable gradient computation method inspired by elementwise layer, this is just for comparison/debugging purposes
+
+			if (propagate_down[0]) {
 				// Gradient with respect to bottom data
 				caffe_div(top[0]->count(), top[0]->cpu_data(), bottom[0]->cpu_data(), bottom[0]->mutable_cpu_diff());
 				caffe_mul(top[0]->count(), bottom[0]->mutable_cpu_diff(), top[0]->cpu_diff(), bottom[0]->mutable_cpu_diff());
+			}
 
-				// Gradient with respect to weights
-				caffe_div(top[0]->count(), top[0]->cpu_data(), blobs_[0]->cpu_data(), this->blobs_[0]->mutable_cpu_diff());
-				caffe_mul(top[0]->count(), this->blobs_[0]->mutable_cpu_diff(), top[0]->cpu_diff(), this->blobs_[0]->mutable_cpu_diff());
+			// Gradient with respect to weights
+			caffe_div(top[0]->count(), top[0]->cpu_data(), blobs_[0]->cpu_data(), this->blobs_[0]->mutable_cpu_diff());
+			caffe_mul(top[0]->count(), this->blobs_[0]->mutable_cpu_diff(), top[0]->cpu_diff(), this->blobs_[0]->mutable_cpu_diff());
 
-				// Gradient with respect to bias
-				if (bias_term_) {
-					LOG(ERROR) << "bias gradient not yet implemented"; // TODO: see elementwise layer for inspiration
-					//caffe_copy(top[0]->count(), top[0]->cpu_diff(), this->blobs_[1]->mutable_cpu_diff()); // = d_i+1
-				}
+			// Gradient with respect to bias
+			if (bias_term_) {
+				LOG(ERROR) << "unstable bias gradient not yet implemented"; // TODO: see elementwise layer for formulas
 			}
 		}
-
-
 	}
 
 #ifdef CPU_ONLY
