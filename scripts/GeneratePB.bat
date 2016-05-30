f7dd296d782a0a7f353d7bdb2f678d4197fd1c83
MalteOeljeklaus <malte.oeljeklaus@tu-dortmund.de>
2016-05-30 12:26:21 +0200

    update to cudnn v5

--- scripts/GeneratePB.bat
+++ scripts/GeneratePB.bat
@@ -4,20 +4,20 @@ if exist ".\3rdparty" (
 	echo found 3rdparty dependencies, all good!
 ) else (
 	echo 3rdparty dependencies not found...
-	if exist ".\cudnn-7.0-win-x64-v4.0-prod.zip" (
+	if exist ".\cudnn-7.5-windows10-x64-v5.0-ga.zip" (
 		call .\tools\wget_1.11.4_cygwin\wget.exe --no-check-cert %thirdpartyurl% -O 3rdparty_tmp.7z
 		call .\tools\7z938-extra\7za.exe x -y 3rdparty_tmp.7z
 		del 3rdparty_tmp.7z
-		call .\tools\7z938-extra\7za.exe e -y cudnn-7.0-win-x64-v4.0-prod.zip -o3rdparty\lib cuda\lib\x64
-		call .\tools\7z938-extra\7za.exe e -y cudnn-7.0-win-x64-v4.0-prod.zip -o3rdparty\bin cuda\bin
-		call .\tools\7z938-extra\7za.exe e -y cudnn-7.0-win-x64-v4.0-prod.zip -o3rdparty\include cuda\include
-		del cudnn-7.0-win-x64-v4.0-prod.zip
+		call .\tools\7z938-extra\7za.exe e -y cudnn-7.5-windows10-x64-v5.0-ga.zip -o3rdparty\lib cuda\lib\x64
+		call .\tools\7z938-extra\7za.exe e -y cudnn-7.5-windows10-x64-v5.0-ga.zip -o3rdparty\bin cuda\bin
+		call .\tools\7z938-extra\7za.exe e -y cudnn-7.5-windows10-x64-v5.0-ga.zip -o3rdparty\include cuda\include
+		del cudnn-7.5-windows10-x64-v5.0-ga.zip
 		copy "%CUDA_PATH_V7_5%\bin\cublas64_75.dll" 3rdparty\bin
 		copy "%CUDA_PATH_V7_5%\bin\cudart64_75.dll" 3rdparty\bin
 		copy "%CUDA_PATH_V7_5%\bin\curand64_75.dll" 3rdparty\bin
 
 	) else (
-		echo ### Please download cudnn-7.0-win-x64-v4.0-prod.zip to your caffe root folder to proceed ###
+		echo ### Please download cudnn-7.5-windows10-x64-v5.0-ga.zip to your caffe root folder to proceed ###
 		exit -1
 	)
 )
